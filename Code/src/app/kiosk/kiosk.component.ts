daa7e2f1720072c698310386bd9df5380f1f3a7f
hsc-soft <hsc.wevois@gmail.com>
2021-06-23 13:02:34 +0530

    icon bounce committed

--- Code/src/app/kiosk/kiosk.component.ts
+++ Code/src/app/kiosk/kiosk.component.ts
@@ -25,6 +25,8 @@ export class KioskComponent {
   allMarkers: any[];
   bounds: any;
   preIndex: any;
+  markerList: any[];
+  kioskMarker: any;
 
 
   constructor(public httpService: HttpClient, private actRoute: ActivatedRoute, private mapService: MapService, private commonService: CommonService) { }
@@ -49,12 +51,32 @@ export class KioskComponent {
     this.getKiosk();
   }
 
+  setHeight() {
+    $('.navbar-toggler').show();
+    $('#divMap').css("height", $(window).height() - 80);
+  }
+
+  setMaps() {
+    var mapstyle = new google.maps.StyledMapType(
+      [
+        {
+          featureType: 'poi',
+          elementType: 'labels',
+          stylers: [{ visibility: "off" }]
+        },
+      ]
+    );
+    let mapProp = this.commonService.initMapProperties();
+    this.map = new google.maps.Map(this.gmap.nativeElement, mapProp);
+    this.map.mapTypes.set('styled_map', mapstyle);
+    this.map.setMapTypeId('styled_map');
+  }
 
   getKiosk() {
     let isFirst = true;
-    this.bounds = new google.maps.LatLngBounds();
     this.kioskList = [];
     this.allMarkers = [];
+    this.markerList = [];
     this.httpService.get(this.kioskUrl).subscribe((res) => {
       let data = res;
       if (res != null) {
@@ -86,48 +108,199 @@ export class KioskComponent {
               if (kioskData[i]["kioskAddress"]["latitude"] != null) {
                 lat = kioskData[i]["kioskAddress"]["latitude"];
                 lng = kioskData[i]["kioskAddress"]["longitude"];
-                let markerUrl = "../assets/img/kiosk.png";
-                let markerList = this.allMarkers;
+                let markerURL = "../assets/img/kiosk.png";
+                this.markerList.push({ id: kioskId, lat: lat, lng: lng, markerURL: markerURL, type: 'kiosk' });
                 this.kioskList.push({ kioskId: kioskId, name: name, address: address, lat: lat, lng: lng, pinCode: pinCode, village: village, mobile: mobile });
-                this.bounds.extend({ lat: Number(lat), lng: Number(lng) });
                 if (isFirst == true) {
                   isFirst = false;
                   $('#divKiosk').show();
                   this.preIndex = 0;
-                  this.setMarker(lat, lng, markerUrl, "kiosk", kioskId, true, 0, markerList);
                   let details = this.kioskList.find(item => item.kioskId == kioskId);
                   if (details != undefined) {
                     this.pageDetail.kioskName = details.name;
                     this.pageDetail.kioskAddress = details.address;
                     this.pageDetail.kioskMobile = details.mobile;
                   }
-                }
-                else {
-                  this.setMarker(lat, lng, markerUrl, "kiosk", kioskId, false, this.allMarkers.length - 1, markerList);
+                  let url = "https://0wybm6aze4.execute-api.ap-south-1.amazonaws.com/prod/kiosk/" + kioskId + "/farmer?limit=0&offset=0";
+                  this.httpService.get(url).subscribe((res) => {
+                    let data = res;
+                    if (res != null) {
+                      this.pageDetail.totalFarmers = Number(data["count"]);
+                    }
+                  })
                 }
               }
             }
           }
-          this.map.fitBounds(this.bounds);
+          this.getMarkerList();
         }
       }
     });
   }
 
-  getFarmers() {
+  getMarkerList() {
     this.bounds = new google.maps.LatLngBounds();
+    if (this.markerList.length > 0) {
+      for (let i = 0; i < this.markerList.length; i++) {
+        let isSelected = false;
+        if (i == 0) {
+          isSelected = true;
+          this.preIndex = 0;
+        }
+        
+        this.setMarker(this.markerList[i]["lat"], this.markerList[i]["lng"], this.markerList[i]["markerURL"], this.markerList[i]["id"], isSelected, i, this.markerList[i]["type"]);
+        this.bounds.extend({ lat: Number(this.markerList[i]["lat"]), lng: Number(this.markerList[i]["lng"]) });
+      }
+    }
+    setTimeout(() => {
+      this.addLisnerOnMarkers(this.allMarkers, this.markerList, this.markerList[0]["type"]);
+    }, 600);    
+    if (this.markerList[0]["type"] == "kiosk") {
+      this.map.fitBounds(this.bounds);
+    }
+  }
+
+
+  setMarker(lat: any, lng: any, markerURL: any, id: any, isSelected: any, index: any, type: any) {
+    let height = 25;
+    let width = 25;
+    if (type == "kiosk") {
+      height = 25;
+      width = 30;
+    }
+    let marker = new google.maps.Marker({
+      position: { lat: Number(lat), lng: Number(lng) },
+      map: this.map,
+      icon: {
+        url: markerURL,
+        fillOpacity: 1,
+        strokeWeight: 0,
+        scaledSize: new google.maps.Size(height, width),
+        origin: new google.maps.Point(0, 0)
+      }
+    });
+    if (isSelected == true) {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+
+    this.allMarkers.push({ marker });
+  }
+
+  addLisnerOnMarkers(markerList: any, list: any, type: any) {
+    if (type == "kiosk") {
+      let kioskList = this.kioskList;
+      let pageDetails = this.pageDetail;
+      let httpServices = this.httpService;
+      let preIndex = this.preIndex;
+      for (let i = 0; i < markerList.length; i++) {
+        markerList[i]["marker"].addListener('click', function () {
+          $('#divKiosk').show();
+          let details = kioskList.find(item => item.kioskId == list[i]["id"]);
+          if (details != undefined) {
+            pageDetails.kioskName = details.name;
+            pageDetails.kioskAddress = details.address;
+            pageDetails.kioskMobile = details.mobile;
+          }
+          let url = "https://0wybm6aze4.execute-api.ap-south-1.amazonaws.com/prod/kiosk/" + list[i]["id"] + "/farmer?limit=0&offset=0";
+          httpServices.get(url).subscribe((res) => {
+            let data = res;
+            if (res != null) {
+              pageDetails.totalFarmers = Number(data["count"]);
+            }
+          });
+          KioskComponent.prototype.setSelectedMarker(i, markerList);
+        });
+      }
+    }
+    else if (type == "farmer") {
+      let details = this.pageDetail;
+      let httpServices = this.httpService;
+      for (let i = 0; i < markerList.length; i++) {
+        markerList[i]["marker"].addListener('click', function () {
+          let url = "https://0wybm6aze4.execute-api.ap-south-1.amazonaws.com/prod/farmer/" + list[i]["id"] + "";
+          httpServices.get(url).subscribe((res) => {
+            let data = res;
+            if (res != null) {
+              $('#divFarmer').show();
+              details.farmerEmail = data["email"];
+              details.farmerName = data["firstName"];
+              if (data["middleName"] != null) {
+                details.farmerName = details.farmerName + " " + data["middleName"]
+              }
+              if (data["lastName"] != null) {
+                details.farmerName = details.farmerName + " " + data["lastName"];
+              }
+              if (data["mobileNumber"] != null) {
+                details.farmerMobile = data["mobileNumber"];
+              }
+              else {
+                details.farmerMobile = "";
+              }
+              if (data["farmerAddress"] != null) {
+                if (data["farmerAddress"]["addressLine1"] != null) {
+                  details.farmerAddress = data["farmerAddress"]["addressLine1"];
+                }
+                if (data["farmerAddress"]["addressLine2"] != null) {
+                  details.farmerAddress += ", " + data["farmerAddress"]["addressLine2"];
+                }
+                if (data["farmerAddress"]["village"] != null) {
+                  details.farmerAddress += ", " + data["farmerAddress"]["village"];
+                }
+                if (data["farmerAddress"]["pincode"] != null) {
+                  details.farmerAddress += " - " + data["farmerAddress"]["pincode"];
+                }
+              }
+              else {
+                details.farmerAddress = "";
+              }
+            }
+          });
+          KioskComponent.prototype.setSelectedMarker(i, markerList);
+        });
+
+      }
+
+    }
+  }
+
+  setSelectedMarker(index: any, markerList: any) {
+    for (let i = 0; i < markerList.length; i++) {
+      if (i == index) {
+        markerList[i]["marker"].setAnimation(google.maps.Animation.BOUNCE);
+      }
+      else {
+        markerList[i]["marker"].setAnimation(null);
+      }
+    }
+  }
+
+
+  getFarmers() {
     let kioskLat = 0;
     let kioskLng = 0;
+    this.markerList = [];
+    this.farmerList=[];
     let kioskDetails = this.kioskList.find(item => item.kioskId == this.selectedKiosk);
     if (kioskDetails != undefined) {
       if (kioskDetails.lat != "") {
         kioskLat = Number(kioskDetails.lat);
         kioskLng = Number(kioskDetails.lng);
-        let markerList = this.allMarkers;
-        let markerUrl = "../assets/img/kiosk.png";
-        this.setMarker(kioskDetails.lat, kioskDetails.lng, markerUrl, "kiosk", this.selectedKiosk, false, -1, markerList);
-        this.bounds.extend({ lat: Number(kioskDetails.lat), lng: Number(kioskDetails.lng) });
-        this.map.fitBounds(this.bounds);
+        let markerURL = "../assets/img/kiosk.png";
+        let marker = new google.maps.Marker({
+          position: { lat: kioskLat, lng: kioskLng },
+          map: this.map,
+          icon: {
+            url: markerURL,
+            fillOpacity: 1,
+            strokeWeight: 0,
+            scaledSize: new google.maps.Size(25, 30),
+            origin: new google.maps.Point(0, 0)
+          }
+        });
+        let centerPoint = new google.maps.LatLng(kioskLat, kioskLng);
+        this.map.setZoom(16);
+        this.map.setCenter(centerPoint);
+        this.kioskMarker = marker;
       }
     }
     this.farmerList = [];
@@ -139,8 +312,7 @@ export class KioskComponent {
         let farmerData = data["rows"];
         if (farmerData.length > 0) {
           for (let i = 0; i < farmerData.length; i++) {
-            let markerUrl = "../assets/img/farmer.png";
-            let contentString = "";
+            let markerURL = "../assets/img/farmer.png";
             let farmerId = farmerData[i]["farmerId"];
             let lat = 0;
             let lng = 0;
@@ -148,10 +320,7 @@ export class KioskComponent {
               if (farmerData[i]["farmerAddress"]["latitude"] != null) {
                 lat = farmerData[i]["farmerAddress"]["latitude"];
                 lng = farmerData[i]["farmerAddress"]["longitude"];
-                let markerList = this.allMarkers;
-                this.setMarker(lat, lng, markerUrl, "farmer", farmerId, false, -1, markerList);
-                this.bounds.extend({ lat: Number(lat), lng: Number(lng) });
-                this.map.fitBounds(this.bounds);
+                this.markerList.push({ id: farmerId, lat: lat, lng: lng, markerURL: markerURL, type: 'farmer' });
               }
               else {
                 const EARTH_RADIUS = 6378;
@@ -161,8 +330,6 @@ export class KioskComponent {
 
                 var u = Math.random();
                 var v = Math.random();
-               // console.log("u" + u);
-               // console.log("v" + v);
 
                 var w = rd * Math.sqrt(u);
                 var t = 2 * Math.PI * v;
@@ -177,35 +344,7 @@ export class KioskComponent {
                 var newlon2 = xp + x0;
                 lat = newlat;
                 lng = newlon;
-
-
-                // distance
-
-              //  var R = 6371; // km
-              //  var dLat = this.toRad(lat - kioskLat);
-             //   var dLon = this.toRad(lng - kioskLng);
-             //   var lat1 = this.toRad(kioskLat);
-              //  var lat2 = this.toRad(lat);
-
-              //  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-             //     Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-              //  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-              //  var d = R * c;
-             //   console.log("distance " + d);
-
-
-
-
-                // Random random = new Random();
-
-                ///  lat = kioskLat + (5 / EARTH_RADIUS) * (180 / Math.PI);
-                //  lng = kioskLng + (5 / EARTH_RADIUS) * (180 / Math.PI) / Math.cos(kioskLat * Math.PI / 180);
-                //console.log("lat:" + lat);
-                // console.log("lng:" + lng);
-                let markerList = this.allMarkers;
-                this.setMarker(lat, lng, markerUrl, "farmer", farmerId, false, -1, markerList);
-                this.bounds.extend({ lat: Number(lat), lng: Number(lng) });
-                this.map.fitBounds(this.bounds);
+                this.markerList.push({ id: farmerId, lat: lat, lng: lng, markerURL: markerURL, type: 'farmer' });
               }
             }
             else {
@@ -231,139 +370,35 @@ export class KioskComponent {
               lat = newlat;
               lng = newlon;
 
-
-
-
-
-              // Random random = new Random();
-
-              ///  lat = kioskLat + (5 / EARTH_RADIUS) * (180 / Math.PI);
-              //  lng = kioskLng + (5 / EARTH_RADIUS) * (180 / Math.PI) / Math.cos(kioskLat * Math.PI / 180);
-              //  console.log("lat:" + lat);
-              //  console.log("lng:" + lng);
-              let markerList = this.allMarkers;
-              this.setMarker(lat, lng, markerUrl, "farmer", farmerId, false, -1, markerList);
-              this.bounds.extend({ lat: Number(lat), lng: Number(lng) });
-              this.map.fitBounds(this.bounds);
+              this.markerList.push({ id: farmerId, lat: lat, lng: lng, markerURL: markerURL, type: 'farmer' });
             }
             this.farmerList.push({ farmerId: farmerId, lat: lat, lng: lng });
           }
+          this.getMarkerList();
         }
       }
     });
 
   }
 
-  toRad(Value: any) {
-    return Value * Math.PI / 180;
-  }
-
-  setMarker(lat: any, lng: any, markerURL: any, type: any, id: any, isSelected: any, index: any, markerList: any) {
-    let height = 25;
-    let width = 25;
-    if (type == "kiosk") {
-      height = 25;
-      width = 30;
-    }
-    let marker = new google.maps.Marker({
-      position: { lat: Number(lat), lng: Number(lng) },
-      map: this.map,
-      icon: {
-        url: markerURL,
-        fillOpacity: 1,
-        strokeWeight: 0,
-        scaledSize: new google.maps.Size(height, width),
-        origin: new google.maps.Point(0, 0)
-      }
-    });
-    if (isSelected == true) {
-      // marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-
-    if (type == "farmer") {
-      let details = this.pageDetail;
-      let httpServices = this.httpService;
-
-      marker.addListener('click', function () {
-        let url = "https://0wybm6aze4.execute-api.ap-south-1.amazonaws.com/prod/farmer/" + id + "";
-        httpServices.get(url).subscribe((res) => {
-          let data = res;
-          if (res != null) {
-            $('#divFarmer').show();
-            details.farmerEmail = data["email"];
-            details.farmerName = data["firstName"];
-            if (data["middleName"] != null) {
-              details.farmerName = details.farmerName + " " + data["middleName"]
-            }
-            if (data["lastName"] != null) {
-              details.farmerName = details.farmerName + " " + data["lastName"];
-            }
-            if (data["mobileNumber"] != null) {
-              details.farmerMobile = data["mobileNumber"];
-            }
-            else {
-              details.farmerMobile = "";
-            }
-            if (data["farmerAddress"] != null) {
-              if (data["farmerAddress"]["addressLine1"] != null) {
-                details.farmerAddress = data["farmerAddress"]["addressLine1"];
-              }
-              if (data["farmerAddress"]["addressLine2"] != null) {
-                details.farmerAddress += ", " + data["farmerAddress"]["addressLine2"];
-              }
-              if (data["farmerAddress"]["village"] != null) {
-                details.farmerAddress += ", " + data["farmerAddress"]["village"];
-              }
-              if (data["farmerAddress"]["pincode"] != null) {
-                details.farmerAddress += " - " + data["farmerAddress"]["pincode"];
-              }
-            }
-            else {
-              details.farmerAddress = "";
-            }
-          }
-        });
-      });
-    }
-    else {
-      let pageDetails = this.pageDetail;
-      let kioskList = this.kioskList;
-
-      marker.addListener('click', function () {
-        // infowindow.open(this.map, marker);
-        $('#divKiosk').show();
-        let details = kioskList.find(item => item.kioskId == id);
-        if (details != undefined) {
-          pageDetails.kioskName = details.name;
-          pageDetails.kioskAddress = details.address;
-          pageDetails.kioskMobile = details.mobile;
-        }
-        //KioskComponent.prototype.setSelectedMarker(index,markerList);
-      });
-    }
-    this.allMarkers.push({ marker });
-  }
-
-  setSelectedMarker(index: any, markerList: any) {
-    // console.log(index);
-    //console.log(markerList.length);
-
-    markerList[index]["marker"].setMap(null);
-
-
-  }
 
   clearAll() {
     this.pageDetail.kioskAddress = "";
     this.pageDetail.kioskMobile = "";
     this.pageDetail.kioskName = "";
     this.pageDetail.totalFarmers = 0;
+    if (this.kioskMarker != null) {
+      this.kioskMarker.setMap(null);
+    }
     $('#divFarmer').hide();
     if (this.allMarkers.length > 0) {
       for (let i = 0; i < this.allMarkers.length; i++) {
         this.allMarkers[i]["marker"].setMap(null);
       }
     }
+    this.allMarkers=[];
+    this.farmerList=[];
+    this.markerList=[];
   }
 
   changeKioskSelection() {
@@ -386,15 +421,13 @@ export class KioskComponent {
   }
 
   setAllkiosk() {
-    this.bounds = new google.maps.LatLngBounds();
     this.farmerList = [];
+    this.markerList = [];
     if (this.kioskList.length > 0) {
       for (let i = 0; i < this.kioskList.length; i++) {
         if (this.kioskList[i]["lat"] != "") {
           let markerUrl = "../assets/img/kiosk.png";
-          let isSelected = false;
           if (i == 0) {
-            isSelected = true;
             $('#divKiosk').show();
             let kioskDetails = this.kioskList.find(item => item.kioskId == this.kioskList[0]["kioskId"]);
             if (kioskDetails != undefined) {
@@ -403,35 +436,11 @@ export class KioskComponent {
               this.pageDetail.kioskMobile = kioskDetails.mobile;
             }
           }
-          let markerList = this.allMarkers;
-          this.setMarker(this.kioskList[i]["lat"], this.kioskList[i]["lng"], markerUrl, "kiosk", this.kioskList[i]["kioskId"], isSelected, 0, markerList);
-          this.bounds.extend({ lat: Number(this.kioskList[i]["lat"]), lng: Number(this.kioskList[i]["lng"]) });
+          this.markerList.push({ id: this.kioskList[i]["kioskId"], lat: this.kioskList[i]["lat"], lng: this.kioskList[i]["lng"], markerURL: markerUrl, type: 'kiosk' });
         }
       }
-      this.map.fitBounds(this.bounds);
+      this.getMarkerList();
     }
-  }
-
-
-  setHeight() {
-    $('.navbar-toggler').show();
-    $('#divMap').css("height", $(window).height() - 80);
-  }
-
-  setMaps() {
-    var mapstyle = new google.maps.StyledMapType(
-      [
-        {
-          featureType: 'poi',
-          elementType: 'labels',
-          stylers: [{ visibility: "off" }]
-        },
-      ]
-    );
-    let mapProp = this.commonService.initMapProperties();
-    this.map = new google.maps.Map(this.gmap.nativeElement, mapProp);
-    this.map.mapTypes.set('styled_map', mapstyle);
-    this.map.setMapTypeId('styled_map');
   }
 }
 
